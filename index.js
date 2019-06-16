dee20d0e4ec66e76db661990df2f950534ed7f81
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-16 19:53:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const returnLastTwoDrivers = function returnLastTwoDrivers(drivers){
   return lastTwo;
 };
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
  function createFareMultiplier(num){
-   return function createFareMultiplier(2)(fare){
+   return function createFareMultiplier(fare){
      return fare*num;
    };
  };
